32e6f38beebc6295432f52860da96c1da9cdd8f9
cronaline <cronaline@gmail.com>
2017-01-14 23:50:27 -0600

    Agregado el metodo creada la clase SkiplistSSet findPredNode

---
+++ SkipList/SkiplistSSet.java
@@ -0,0 +1,20 @@
+/*
+SkiplistSSet
+Version 1
+14/enero/2017
+Skiplist que implementa la interfaz de un SSet
+*/
+
+public class SkiplistSSet{
+        Node findPredNode(int i){
+            Node u = sentinel;
+            int r =h;
+            while(r >= 0){
+                while(u.next[r] != null && compare(u.next[r].x, x) < 0){
+                    u = u.next[r];
+                }
+                r--;
+            }
+            return u;
+        }
+}
