0c79946cd30e7878d54f3ae46d13c52e5298da59
cronaline <cronaline@gmail.com>
2017-01-19 23:46:08 -0600

    Agregados los metodos listar y size

--- LinkedList/DLList.java
+++ LinkedList/DLList.java
@@ -4,12 +4,15 @@ Doubly-Linked List
 Version 1
 12/enero/2017
 Lista doblemente ligada, cuenta con un nodo llamado dummy que une el inicio de la lista con el final de la misma
-con el obejtivo de facilitar las operaciones
+con el obejtivo de facilitar las operaciones. Esta lista doblemente ligada implementa las operaciones de la pila
+y la cola
 */
 
 //Clase Nodo que sera utilizada en la lista doblemente ligada
 //Contiene el valor almacenado en el Nodo, asi como las referencias
 //al nodo anterior y al siguiente
+import java.util.Scanner;
+
 class Node{
     int x;
     Node prev, next;
@@ -27,6 +30,52 @@ class DLList{
         n = 0;
     }
 
+    public static void main(String args[]){
+        int op, in, val;
+        DLList dl = new DLList();
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("\nElija la accion a realizar");
+            System.out.println("1) para obtener el tamaño del Array\n"
+                +"2) para realizar una operacion push\n"
+                +"3) para realizar una operacion pop\n"
+                +"4) para realizar una operacion add\n"
+                +"5) para realizar una operacion remove\n"
+                +"6) para mostrar los elementos de la lista lista\n"
+                +"0) para terminar el programa"
+            );
+            op = sc.nextInt();
+            switch(op){
+                case 1:
+                    System.out.println("Tamaño del arreglo: "+sl.size());
+                break;
+                case 2:
+                    System.out.println("Ingrese el valor que desea insertar");
+                    val = sc.nextInt();
+                    sl.push(val);
+                break;
+                case 3:
+                    System.out.println("El valor almacenado en la cabeza de la lista es: "+sl.pop());
+                break;
+                case 4:
+                    System.out.println("Ingrese el nuevo valor");
+                    val = sc.nextInt();
+                    sl.add(val);
+                break;
+                case 5:
+                    System.out.println("El valor eliminado fue: "+sl.remove());
+                break;
+                case 6:
+                    System.out.println();
+                    sl.listar();
+                break;
+            }
+        }while(op != 0);
+    }
+
+    int size(){
+        return n;
+    }
     //Metodo que encuentra un nodo de acuerdo a su posicion en la lista
     //Entrada: posicion en la lista
     Node getNode(int i){
@@ -97,5 +146,17 @@ class DLList{
         Node w = getNode(i);
         remove(w);
         return w.x;
+    }
+
+    void lista(){
+        for(Node i = dummy.next; i != dummy; i=i.next){
+            System.out.print(i.x+" ");
+        }
+    }
+
+    void listaAlReves(){
+        for(Node i = dummy.prev; i != dummy; i=i.prev){
+            System.out.print(i.x+" ");
+        }
     }
 }
