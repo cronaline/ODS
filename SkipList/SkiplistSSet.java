bce133d351b44b98edcf4a0dbbb3a5dda7235202
cronaline <cronaline@gmail.com>
2017-01-20 23:20:55 -0600

    Arreglada el metodo que genera numeros aleatorios

--- SkipList/SkiplistSSet.java
+++ SkipList/SkiplistSSet.java
@@ -6,31 +6,32 @@ Skiplist que implementa la interfaz de un SSet
 */
 import java.util.Random;
 public class SkiplistSSet{
-        private Node findPredNode(int i){
-            Node u = sentinel;
-            int r =h;
-            while(r >= 0){
-                while(u.next[r] != null && compare(u.next[r].x, x) < 0){
-                    u = u.next[r];
-                }
-                r--;
+    Random rand = new Random();
+    private Node findPredNode(int i){
+        Node u = sentinel;
+        int r =h;
+        while(r >= 0){
+            while(u.next[r] != null && compare(u.next[r].x, x) < 0){
+                u = u.next[r];
             }
-            return u;
+            r--;
         }
+        return u;
+    }
 
-        public int find(int val){
-            Node u = findPredNode(val);
-            return u.next[0] == null ? null : u.next[0].x;
-        }
+    public int find(int val){
+        Node u = findPredNode(val);
+        return u.next[0] == null ? null : u.next[0].x;
+    }
 
-        int pickHeight(){
-            int z = rand.nextInt();
-            int k = 0;
-            int m = 1;
-            while((z & m) != 0){
-                k++;
-                m <<= 1;
-            }
-            return k;
+    int pickHeight(){
+        int z = rand.nextInt();
+        int k = 0;
+        int m = 1;
+        while((z & m) != 0){
+            k++;
+            m <<= 1;
         }
+        return k;
+    }
 }
