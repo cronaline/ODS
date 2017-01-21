0c39570306ea4bc5a2ed6872f0743f6b0abf8970
cronaline <cronaline@gmail.com>
2017-01-20 23:51:56 -0600

    Agregado el metodo add

--- SkipList/SkiplistSSet.java
+++ SkipList/SkiplistSSet.java
@@ -34,4 +34,25 @@ public class SkiplistSSet{
         }
         return k;
     }
+
+    boolean add(int x){
+        Node u = sentinel;
+        int r = h;
+        int comp = 0;
+        while(r >= 0){
+            while(u.next[r] != null && (comp = compare(u.next[r].x, x)) < 0){
+                u = u.next[r];
+            }if(u.next[r] != null && comp == 0) return false;
+            stack[r--] = u; //hacia abajo, guarda u
+        }
+        Node w = new Node(x, pickHeight());
+        while(h < w.height()){
+            stack[++h] = sentinel; //incrementa altura
+        }for(int i = 0; i < w.next.length; i++){
+            w.next[i] = stack[i].next[i];
+            stack[i].next[i] = w;
+        }
+        n++;
+        return true;
+    }
 }
