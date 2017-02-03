a3360f444c44e544d70bd5cd7c0f4824cbd5930d
cronaline <cronaline@gmail.com>
2017-02-02 23:36:14 -0600

    Fucionada la clase nodo y SkiplistSSet en una sola clase, todas las modificaciones necesarias fueron hechas

--- SkipList/SkiplistSSet.java
+++ SkipList/SkiplistSSet.java
@@ -5,7 +5,24 @@ Version 1
 Skiplist que implementa la interfaz de un SSet
 */
 import java.util.Random;
-public class SkiplistSSet{
+import java.lang.reflect.Array;
+
+class Node{
+    int x;
+    Node next [];
+
+    //Constructor de la clase nodo, recibe el valor a almacenar en el nodo, asi como su altura
+    Node(int ix, int h){
+        x = ix;
+        next = Array.newInstance(Node.class, h+1);
+    }
+
+    //Metodo que retorna la altura del nodo
+    int height(){
+        return next.length - 1;
+    }
+}
+class SkiplistSSet{
     Random rand = new Random();
     private Node findPredNode(int i){
         Node u = sentinel;
